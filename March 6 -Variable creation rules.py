f7a0fa8b0fa4a35550b86811142589525ec6ddd8
SatyaMiryala <98235590+SatyaMiryala@users.noreply.github.com>
2022-03-12 12:24:06 +0530

    Add files via upload

---
+++ March 6 -Variable creation rules.py
@@ -0,0 +1,34 @@
+#Variable creation rules ===> programs for this one
+
+#1. A variable name must start with a letter or the underscore character
+
+
+Python_programing = "New"
+
+print(Python_programing)
+
+_Python = 109
+
+print(_Python)
+
+#2. A variable name cannot start with a number
+
+8python = 90 #Invalid variable syntax
+
+
+#3. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+
+Python@ = 50 #Invalid variable syntax
+
+#4. Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+
+Satya = " One Variable"
+
+satya = "other Variale"
+
+SATYA = "another varible"
+
+print(satya)
+print(Satya)
+print(SATYA)
