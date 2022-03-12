6c2dbd65fbadb78e8324ec1a21b2897979b1fde3
SatyaMiryala <98235590+SatyaMiryala@users.noreply.github.com>
2022-03-12 12:09:29 +0530

    Add files via upload

---
+++ March 6 Task-compiler and interpreter.py
@@ -0,0 +1,21 @@
+#Difference between compiler and interpreter
+'''
+Both compilers and interpreters are used to convert a program written in a
+high-level language into machine code understood by computers.
+
+Interperters:
+1. reads the program line to line checking for errors, and runs the
+    program simultaneously.
+
+2. Interpreters usually take less amount of time to analyze the source code.
+    However, the overall execution time is comparatively slower than compilers.
+
+3. Programming languages like JavaScript, Python, Ruby use interpreters.
+
+Compliers
+1. It analyses the entire program and converts it into machine code.
+
+2. Compilers usually take a large amount of time to analyze the source code.
+    However, the overall execution time is comparatively faster than interpreters.
+
+3. Programming languages like C, C++, Java use compilers.
