4268a6fd3e5345d929cc9f9aa845c96167ea99e2
Satya Anvesh <98235590+SatyaMiryala@users.noreply.github.com>
2022-03-13 11:50:54 +0530

    Add files via upload

---
+++ March 12 Datatype conversion.py
@@ -0,0 +1,95 @@
+#Datatype conversion
+
+#int ==> Float, bool & string
+a = 10
+
+print(a)
+print(type(a))
+
+b = float(a)
+
+print(b)
+print(type(b))
+
+c = bool(a)
+
+print(c)
+print(type(c))
+
+d = str(a)
+
+print(d)
+print(type(d))
+'''
+
+#String ==> int, Float & bool
+
+string1 = "Python"
+
+print(string1)
+print(type(string1))
+
+string2 = float(string1)
+
+print(string2)
+print(type(string2))
+
+string13 = bool(string1)
+
+print(string3)
+print(type(string3))
+
+string4 = int(string1)
+
+print(string4)
+print(type(string4))
+'''
+#Bool ==> Int, Float and String
+
+bool_1 = True
+
+print(bool_1)
+print(type(bool_1))
+
+bool_2 = int(bool_1)
+
+print(bool_2)
+print(type(bool_2))
+
+bool_3 = float(bool_1)
+
+print(bool_3)
+print(type(bool_3))
+
+bool_4 = str(bool_1)
+
+print(bool_4)
+print(type(bool_4))
+
+#Float ==> Int, Bool and String
+
+float_1 = 34.8924
+
+print(float_1)
+print(type(float_1))
+
+float_2 = int(float_1)
+
+print(float_2)
+print(type(float_2))
+
+float_3 = bool(float_1)
+
+print(float_3)
+print(type(float_3))
+
+float_4 = str(float_1)
+
+print(float_4)
+print(type(float_4))
+
+
+
+
+
+
